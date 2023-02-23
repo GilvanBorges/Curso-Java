43a0f324068ae7a758bc1544a78af1b8ad9dc6ea
Gilvan Borges dos Santos <gilvan.dev.santos@gmail.com>
2023-02-23 13:48:39 -0300

    tipos

--- tipos-de-variaveis/src/TiposEvariaveis.java
+++ tipos-de-variaveis/src/TiposEvariaveis.java
@@ -1,5 +1,6 @@
 public class TiposEvariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
     }
 }
