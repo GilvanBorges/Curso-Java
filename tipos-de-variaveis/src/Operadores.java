b7441d3aba8812e8853751f349291c0344be99df
Gilvan Borges dos Santos <gilvan.dev.santos@gmail.com>
2023-02-23 14:20:54 -0300

    operador ternario

--- tipos-de-variaveis/src/Operadores.java
+++ tipos-de-variaveis/src/Operadores.java
@@ -1,8 +1,13 @@
 public class Operadores {
     public static void main(String[] args) {
-        int numero = 5;
+        /*int numero = 5;
         System.out.println(++numero);
         boolean variavel = false;
-        System.out.println(!variavel);
+        System.out.println(!variavel); */
+        int a=6;
+        int b=6;
+
+        int resultado = a == b ? 1 : 0;
+            System.out.println(resultado);
     }
 }
