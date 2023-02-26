78ea57fb1fe8a9499697b4ad537b7f9bc9f5576c
Gilvan Borges dos Santos <gilvan.dev.santos@gmail.com>
2023-02-26 13:07:25 -0300

    exer-06

---
+++ Loops_exercicios/src/fatorial.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class fatorial {
+    public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Fatorial: ");
+
+            int fatorial = scanner.nextInt();
+            int multiplicacao = 1;
+
+         
+
+            for(int i = fatorial;i >= 1; i= i-1){
+                multiplicacao = multiplicacao * i;
+            }
+            System.out.println(fatorial + "! = " + multiplicacao);
+        }
+    }
