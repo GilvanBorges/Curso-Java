28f5e650e76c4220b3b42abc06fae8e305335266
Gilvan Borges dos Santos <gilvan.dev.santos@gmail.com>
2023-02-26 11:49:12 -0300

    aprimorando

--- Loops_exercicios/src/Nota.java
+++ Loops_exercicios/src/Nota.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class Nota {
     public static void main(String[] args) {
-        Scanner scanner =new Scanner(System.in);
-        
-        int nota;
-        System.out.println("Nota: ");
-        nota = scanner.nextInt();
+        try (Scanner scanner = new Scanner(System.in)) {
+            int nota;
+            System.out.println("Nota: ");
+            nota = scanner.nextInt();
 
 
-        while(nota < 0 || nota > 10){
-                System.out.println("Nota inválida, Digite novamente. ");
-                nota = scanner.nextInt();
+            while(nota < 0 || nota > 10){
+                    System.out.println("Nota inválida, Digite novamente. ");
+                    nota = scanner.nextInt();
+            }
         }     
     }
 }
