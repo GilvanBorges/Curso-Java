1ddf80be46b02fe89f3d01e04d13adc239f44c19
Gilvan Borges dos Santos <gilvan.dev.santos@gmail.com>
2023-02-26 14:02:16 -0300

    arrays3

---
+++ Loops_exercicios/src/ArrayConsoante.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class ArrayConsoante {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] consoantes = new String[6];
+        int quantidadeConsoante = 0;
+
+        int count = 0;
+        do{
+            System.out.println("Digite uma letra: ");
+            String letra =scanner.next();
+
+            if(!(letra.equalsIgnoreCase("a") |
+                 letra.equalsIgnoreCase("e") |
+                 letra.equalsIgnoreCase("i") |
+                 letra.equalsIgnoreCase("o") |
+                 letra.equalsIgnoreCase("u"))) {
+            consoantes[count] = letra;
+            quantidadeConsoante++;
+                 }
+                 count++;
+        }while(count < consoantes.length);
+
+       for (String consoante: consoantes) {
+            if (consoante != null)
+            System.out.println(consoante + " ");
+            
+       }
+       System.out.println("Quantidade de consoantes: " + quantidadeConsoante);
+        System.out.println(consoantes.length);
+    }
+}
