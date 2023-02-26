6c8c2745d406694bad9f6ed70348d84d408e0f63
Gilvan Borges dos Santos <gilvan.dev.santos@gmail.com>
2023-02-26 11:19:10 -0300

    loops

---
+++ Logica-Condicional-Controle-de-Fluxos-em-Java/src/Aula02.java
@@ -0,0 +1,10 @@
+public class Aula02 {
+    public static void main(String[] args) throws Exception {
+        /*int n1, n2, r;
+        n1 = 5;
+        n2 = 9;
+        r = (n1>n2)?0:1;
+        System.out.println(r);*/
+        System.out.println(true||false);
+    }
+}
